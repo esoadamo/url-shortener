8e1e5fdcdd2bec3b8a41303d6a08787157fce0a0
Adam Hlaváček <esoadamo@gmail.com>
2017-05-28 15:31:28 +0200

    Fixed typo(s) caused by fixing typo(s)

--- shortener_client.py
+++ shortener_client.py
@@ -4,7 +4,7 @@ import urllib.parse
 
 def short_url(shortener_url, long_url, api_key):
     """
-    Shortens am url
+    Shortens an url
     :param shortener_url: url of your shortener script
     :param long_url: the url you want to shorten
     :param api_key: your key set in settings
@@ -13,6 +13,6 @@ def short_url(shortener_url, long_url, api_key):
     r = urllib.request.urlopen(shortener_url +
                                "/?link=" + urllib.parse.quote(long_url) +
                                '&key=' + urllib.parse.quote(api_key)).read().decode("UTF8")
-    if r.startswith('Setting are not completed') or r.startswith("Wrong API key"):
+    if r.startswith('Setting is not completed') or r.startswith("Wrong API key"):
         raise ValueError(r)
     return r
